77a9d241626b55e291b3b3ec0a0c8943c6e28cfc
Jacob Lockhart <procoder165@gmail.com>
2023-01-26 10:26:02 -0500

    2nd commit:

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from website import create_app
+import os
 
 #Create instance of app
 app = create_app()
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run(debug=True, port=os.getenv("PORT", default=3001))
